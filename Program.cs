4b3a5ba2516f07e73b11439073f14f0433519831
Valentina <valya.romanchuk3211@gmail.com>
2018-09-15 23:15:04 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Valya", "Romanchuk", Gender.female);
+            person.print();
+            //Console.WriteLine(person);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform_task1
+{
+    class Teacher:Person
+    {
+        public int Salery { get; set; }
+        List<Student> studentss = new List<Student>();
+        
+        private int used;
+        private Student[] students;
+        private void checkMem()
+        {
+            Student[] s = new Student[students.Length];
+            for (int i = 0; i < students.Length; ++i)
+            {
+                s[i] = students[i];
+                students = s;
+            }
+        }
+        Teacher(int salery):base("Olga","Mechnikova",Gender.female)
+        {
+            Salery = salery;
+        }
+
+         public override void print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
